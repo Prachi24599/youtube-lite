6c9b680ed402233be3fd62c95c7306ed4ffc9d47
Prachi24599 <prachi24599@gmail.com>
2023-06-25 01:16:13 +0530

    Left Nav menu added in Feed section

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { Context } from "../context/contextApi";
+import LeftNav from "./LeftNav";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <div className="flex flex-row h-[calc(100%-56px)]">
+      <LeftNav />
+    </div>
+  );
 };
 
 export default Feed;
--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import LeftNavMenuItem from "./LeftNavMenuItem";
+import { categories } from "../utils/constants";
+import { Context } from "../context/contextApi";
 
 const LeftNav = () => {
-  return <div>LeftNav</div>;
+  const { selectCategories, setSelectCategories, mobileMenu } =
+    useContext(Context);
+  return (
+    
+  );
 };
 
 export default LeftNav;
