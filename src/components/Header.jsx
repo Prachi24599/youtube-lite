2d9c62cc885fd0b50b44d15c878aa4fc2493191d
Prachi24599 <prachi24599@gmail.com>
2023-06-24 22:19:09 +0530

    loader added in header with navigation details

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+import ytLogo from "../images/yt-logo.png";
+import ytLogoMobile from "../images/yt-logo-mobile.png";
+
+import { SlMenu } from "react-icons/sl";
+import { IoIosSearch } from "react-icons/io";
+import { RiVideoAddLine } from "react-icons/ri";
+import { FiBell } from "react-icons/fi";
+import { CgClose } from "react-icons/cg";
+
+import { Context } from "../context/contextApi";
+import Loader from "../shared/Loader";
 
 const Header = () => {
-  return <div>Header</div>;
+  const [searchQuery, setSearchQuery] = useState("");
+  const { loading, mobileMenu, setMobileMenu } = useContext(Context);
+  const navigate = useNavigate();
+
+  const searchQueryHandler = (event) => {
+    if (
+      (event?.key === "Enter" || event === "searchButton") &&
+      searchQuery?.length > 0
+    ) {
+      navigate(`/searchResult/${searchQuery}`);
+    }
+  };
+
+  const mobileMenuToggle = () => {
+    setMobileMenu(!mobileMenu);
+  };
+
+  const { pathname } = useLocation();
+  const pageName = pathname?.split("/")?.filter(Boolean)?.[0];
+
+  return (
+    <div className="sticky top-0 z-10 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-slate-500 dark:bg-black">
+      {loading && <Loader />}
+    </div>
+  );
 };
 
 export default Header;
