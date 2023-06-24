f144211c25c70fa1594e08b915dff13bba0e3d8e
Prachi24599 <prachi24599@gmail.com>
2023-06-25 01:17:02 +0530

    Left Nav section created

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -8,7 +8,27 @@ const LeftNav = () => {
   const { selectCategories, setSelectCategories, mobileMenu } =
     useContext(Context);
   return (
-    
+    <div className="md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240] md:translate-x-0 transition-all">
+      <div className="flex px-5 flex-col">
+        {categories.map((item) => {
+          return (
+            <React.Fragment>
+              <LeftNavMenuItem
+                type={item.type === "home" ? "Home" : item.name}
+                icon={item.icon}
+                action={() => {}}
+                className={`${
+                  selectCategories === item.name ? "bg-white/[0.15]" : ""
+                }`}
+              />
+              {item.divider && <hr className="my-5 border-white/[0.2]" />}
+            </React.Fragment>
+          );
+        })}
+        <hr className="my-5 border-white/[0.2]" />
+        <div className="text-white/[0.5] text-12px">Clone By : Prachi</div>
+      </div>
+    </div>
   );
 };
 
