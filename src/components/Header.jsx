44a9a7a46a07df7c7e018ef6dc23e4bfe7b8713f
Prachi24599 <prachi24599@gmail.com>
2023-06-30 17:43:21 +0530

    handle search result on button click

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -69,7 +69,10 @@ const Header = () => {
             value={searchQuery}
           />
         </div>
-        <button className="w-[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border-l-0 border-[#303030] rounded-r-3xl bg-white/[0.1]">
+        <button
+          onClick={() => searchQueryHandler("searchButton")}
+          className="w-[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border-l-0 border-[#303030] rounded-r-3xl bg-white/[0.1]"
+        >
           <IoIosSearch className="text-white text-xl" />
         </button>
       </div>
