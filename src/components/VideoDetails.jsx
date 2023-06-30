1130a0685300f31a8b16876b35a1ddb21392d502
Prachi24599 <prachi24599@gmail.com>
2023-06-30 15:48:09 +0530

    video autoplay on default

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -49,6 +49,7 @@ const VideoDetails = () => {
               width="100%"
               height="100%"
               style={{ backgroundColor: "#000000" }}
+              playing={true}
             />
           </div>
           <div className="text-white font-bold text-sm md:text-xl mt-4 line-clamp-2">
